2dede5647972bfaec690e0dc6ec8d04f9d2bed20
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-05-15 13:16:41 -0300

    GateInMemory implemenation

---
+++ src/main/java/br/eti/clairton/security/GateInMemory.java
@@ -0,0 +1,36 @@
+package br.eti.clairton.security;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.enterprise.inject.Vetoed;
+import javax.validation.constraints.NotNull;
+
+@Vetoed
+public class GateInMemory implements Gate {
+	private final Map<String, Map<String, Map<String, List<String>>>> authorizations;
+
+	public GateInMemory() {
+		this(new HashMap<String, Map<String, Map<String, List<String>>>>());
+	}
+
+	public GateInMemory(
+			Map<String, Map<String, Map<String, List<String>>>> authorizations) {
+		super();
+		this.authorizations = authorizations;
+	}
+
+	@Override
+	public Boolean isOpen(@NotNull final String user,
+			@NotNull final String app, @NotNull final String resource,
+			@NotNull final String operation) {
+		if (user != null) {
+			return authorizations.get(user).get(app).get(resource)
+					.contains(operation);
+		} else {
+			return Boolean.FALSE;
+		}
+	}
+
+}
--- src/test/java/br/eti/clairton/security/Produce.java
+++ src/test/java/br/eti/clairton/security/Produce.java
@@ -1,5 +1,10 @@
 package br.eti.clairton.security;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
@@ -44,5 +49,24 @@ public class Produce {
 	@Produces
 	public Locksmith getLocksmith(@Default Lock lock) {
 		return new LocksmithInMemory(lock);
+	}
+
+	@Produces
+	public Gate getGate() {
+		final Map<String, Map<String, List<String>>> roles = new HashMap<String, Map<String, List<String>>>() {
+			private static final long serialVersionUID = 1L;
+
+			{
+				put("Pass", new HashMap<String, List<String>>() {
+					private static final long serialVersionUID = 1L;
+					{
+						put("aplicacao", Arrays.asList("create", "update"));
+					}
+				});
+			}
+		};
+		Map<String, Map<String, Map<String, List<String>>>> authorizations = new HashMap<String, Map<String, Map<String, List<String>>>>();
+		authorizations.put("admin", roles);
+		return new GateInMemory();
 	}
 }
