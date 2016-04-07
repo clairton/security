b1e45c0fade68698de1e421e27ec79d333d27ef8
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2016-04-07 10:59:36 -0300

    Create exception password policy

---
+++ src/main/java/br/eti/clairton/security/PasswordPolicyException.java
@@ -0,0 +1,10 @@
+package br.eti.clairton.security;
+
+/**
+ * Exception to password policy not satisfied.
+ * 
+ * @author Clairton Rodrigo Heinzen<clairton.rodrigo@gmail.com>
+ */
+public class PasswordPolicyException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+}
